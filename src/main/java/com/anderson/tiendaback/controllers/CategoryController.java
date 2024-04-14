33b950b317a3ba2038915a091a086e7f835963c4
Anderson <andersoncusma20@gmail.com>
2024-04-14 13:06:46 -0500

    Payment Controller1

---
+++ src/main/java/com/anderson/tiendaback/controllers/CategoryController.java
@@ -0,0 +1,5 @@
+package com.anderson.tiendaback.controllers;
+
+public class CategoryController {
+
+}
--- src/main/java/com/anderson/tiendaback/controllers/PaymentController.java
+++ src/main/java/com/anderson/tiendaback/controllers/PaymentController.java
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.anderson.tiendaback.Mapper;
 import com.anderson.tiendaback.exception.ResourceNotFoundException;
@@ -25,6 +27,8 @@ import com.anderson.tiendaback.viewModels.PaymentViewModel;
 
 import jakarta.validation.ValidationException;
 
+@RestController
+@RequestMapping("/api/payments")
 public class PaymentController {
 
 	@Autowired
