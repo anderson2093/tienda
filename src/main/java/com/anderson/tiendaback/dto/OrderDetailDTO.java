79186d9bfb9b7962f80924e3692dd1da2f7aa949
Anderson <andersoncusma20@gmail.com>
2024-04-25 14:11:00 -0500

    Implement lombok

--- src/main/java/com/anderson/tiendaback/dto/OrderDetailDTO.java
+++ src/main/java/com/anderson/tiendaback/dto/OrderDetailDTO.java
@@ -1,10 +1,8 @@
 package com.anderson.tiendaback.dto;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @Getter
